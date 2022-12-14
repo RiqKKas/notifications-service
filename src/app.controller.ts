8b0ef465c9814bb0dcf76eadacdfa8e754ecf317
the-riquelme <riquelmedamiaosilva@gmail.com>
2022-12-14 18:15:00 -0300

    :bricks: ci: integrates nestjs with prisma

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Post } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { randomUUID } from 'node:crypto';
 
-@Controller()
+@Controller('notifications')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create() {
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content: 'This is a message...',
+        category: 'social',
+        recipientId: randomUUID(),
+      },
+    });
   }
 }
