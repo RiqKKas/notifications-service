2ca0c02012ce0e0b2c4c236b5ca272f499ca0b27
the-riquelme <riquelmedamiaosilva@gmail.com>
2022-12-17 19:52:36 -0300

    :sparkles: feat: create notification entity

---
+++ src/app/entities/notification.ts
@@ -0,0 +1,59 @@
+export interface NotificationProps {
+  recipientId: string;
+  content: string;
+  category: string;
+  readAt?: Date | null;
+  createdAt: Date;
+}
+
+export class Notification {
+  private props: NotificationProps;
+
+  constructor(props: NotificationProps) {
+    this.props = props;
+  }
+
+  public get recipientId(): string {
+    return this.props.recipientId;
+  }
+
+  public set recipientId(recipientId: string) {
+    this.props.recipientId = recipientId;
+  }
+
+  public get content(): string {
+    return this.props.content;
+  }
+
+  public set content(content: string) {
+    this.props.content = content;
+  }
+
+  public get category(): string {
+    return this.props.category;
+  }
+
+  public set category(category: string) {
+    this.props.category = category;
+  }
+
+  public read() {
+    this.props.readAt = new Date();
+  }
+
+  public unread() {
+    this.props.readAt = null;
+  }
+
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
+
+  public set readAt(readAt: Date) {
+    this.props.readAt = readAt;
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt;
+  }
+}
