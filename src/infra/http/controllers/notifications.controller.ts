e6c3ac3500d9253b041286f4f3a70014cd451be8
the-riquelme <riquelmedamiaosilva@gmail.com>
2022-12-18 15:33:25 -0300

    :sparkles: feat: create notification view model

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateNotificationBody } from '../dtos/create-notification-body';
 import { SendNotification } from '@app/use-cases/send-notification';
+import { NotificationViewModel } from '@infra/view-models/notification-view-model';
 
 @Controller('notifications')
 export class NotificationsController {
@@ -17,7 +18,7 @@ export class NotificationsController {
     });
 
     return {
-      notification,
+      notification: NotificationViewModel.toHTTP(notification),
     };
   }
 }
