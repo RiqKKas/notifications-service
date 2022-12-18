bba95ea53a98fb28ee338fa870a6803835373896
the-riquelme <riquelmedamiaosilva@gmail.com>
2022-12-18 19:41:47 -0300

    :sparkles: feat: add toDomain mapper

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,3 +1,5 @@
+import { Notification as RawNotification } from '@prisma/client';
+import { Content } from '@app/entities/content';
 import { Notification } from '@app/entities/notification';
 
 export class PrismaNotificationMapper {
@@ -10,5 +12,19 @@ export class PrismaNotificationMapper {
       readAt: notification.readAt,
       createdAt: notification.createdAt,
     };
+  }
+
+  static toDomain(raw: RawNotification): Notification {
+    return new Notification(
+      {
+        category: raw.category,
+        content: new Content(raw.content),
+        recipientId: raw.recipientId,
+        readAt: raw.readAt,
+        canceledAt: raw.canceledAt,
+        createdAt: raw.createdAt,
+      },
+      raw.id,
+    );
   }
 }
