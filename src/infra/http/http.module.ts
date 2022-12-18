5cdf5765c922a93cd46858b7a007afea904626f0
the-riquelme <riquelmedamiaosilva@gmail.com>
2022-12-17 22:36:41 -0300

    :sparkles: feat: connect infra modules

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { NotificationsController } from './controllers/notifications.controller';
+import { SendNotification } from '../../app/use-cases/send-notification';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule],
   controllers: [NotificationsController],
+  providers: [SendNotification],
 })
 export class HttpModule {}
